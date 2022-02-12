5af5a7ba94e8e828cdb3ae2c385e0776d22d9d09
igor-chazov <igor-chazov@yandex.ru>
2022-02-12 19:09:24 +0500

    Выполнено ДЗ

---
+++ src/epics/index.js
@@ -0,0 +1,35 @@
+import { ofType } from 'redux-observable';
+import { ajax } from 'rxjs/ajax';
+import { map, mergeMap, retry, debounceTime, switchMap, catchError } from 'rxjs/operators';
+import { CHANGE_SEARCH_FIELD, SEARCH_SKILLS_REQUEST } from '../actions/actionTypes';
+import { searchSkillsRequest, searchSkillsSuccess, searchSkillsFailure, resetSkills } from '../actions/actionCreators';
+import { of } from 'rxjs';
+import links from '../Utility/links';
+
+export const changeSearchEpic = (action$) =>
+  action$.pipe(
+    ofType(CHANGE_SEARCH_FIELD),
+    map((o) => o.payload.search.trim()),
+    debounceTime(100),
+    mergeMap((o) => {
+      if (o === '') {
+        return of(resetSkills());
+      } else {
+        return of(searchSkillsRequest(o));
+      }
+    })
+  );
+
+export const searchSkillsEpic = (action$) =>
+  action$.pipe(
+    ofType(SEARCH_SKILLS_REQUEST),
+    map((o) => o.payload.search),
+    map((o) => new URLSearchParams({ q: o })),
+    switchMap((o) =>
+      ajax.getJSON(`${links.search}?${o}`).pipe(
+        retry(3),
+        map((o) => searchSkillsSuccess(o)),
+        catchError((e) => of(searchSkillsFailure(e)))
+      )
+    )
+  );
